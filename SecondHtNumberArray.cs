c1eb7215f99926e9f93f6809f8d503f14a85e548
Laxmikanta Jena <laxmikantajena30@gmail.com>
2025-03-18 18:24:25 +0530

    18/03/2025

---
+++ SecondHtNumberArray.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROGRAMMINGPART
+{
+    internal class SecondHtNumberArray
+    {
+        public static void Main(string[] args)
+        {
+            int[] arr = { 10, 26, 45, 10, 15, 18, 78 };
+            int max = int.MinValue;
+            int secondmax = int.MinValue;
+            foreach (int num in arr)
+            {
+                if (num > max)
+                {
+                    secondmax = max;
+                    max = num;
+                }
+                else if (num > secondmax && num != secondmax)
+                {
+                    secondmax = num;
+                }
+                Console.WriteLine("Second highest one" + secondmax);
+                Console.WriteLine("Highest Number one" + max);
+            }
+            Console.WriteLine("Second Max Number  :" + secondmax);
+            Console.WriteLine("Highest Number  :" + max);
+        }
+    }
+}
